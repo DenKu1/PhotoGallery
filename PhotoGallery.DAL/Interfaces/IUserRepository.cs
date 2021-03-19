bb9d6920f561fd69eee80d8c0ab8db16cd33fe51
DenKu1 <ledar54@gmail.com>
2021-03-19 18:37:30 +0200

    Add entity repositories interfaces

---
+++ PhotoGallery.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PhotoGallery.DAL.Entities;
+
+namespace PhotoGallery.DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+
+        Task<User> GetByUserNameAsync(string userName);
+
+        Task<User> GetByIdAsync(int id);
+    }
+}
