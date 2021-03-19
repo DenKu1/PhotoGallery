ea1a9eb87fa642d5ec7db14da55316dd3b885567
gtm417 <Tymofii_Hodik@Epam.com>
2021-03-19 21:37:29 +0200

    add repositories implementation

---
+++ PhotoGallery.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.DAL.EF;
+using PhotoGallery.DAL.Entities;
+using PhotoGallery.DAL.Interfaces;
+
+namespace PhotoGallery.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly GalleryContext _context;
+
+        public UserRepository(GalleryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetByUserNameAsync(string userName)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.UserName == userName);
+        }
+    }
+}
