9e58b290f67e4bd5a3e8c1cd0c6fa9f19190bcd9
gtm417 <Tymofii_Hodik@Epam.com>
2021-03-21 11:48:27 +0200

    add service implementation

---
+++ PhotoGallery.BLL/Services/CommentService.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using PhotoGallery.BLL.DTO;
+using PhotoGallery.BLL.Exceptions;
+using PhotoGallery.BLL.Interfaces;
+using PhotoGallery.DAL.Entities;
+using PhotoGallery.DAL.Interfaces;
+
+namespace PhotoGallery.BLL.Services
+{
+    public class CommentService : Service, ICommentService
+    {
+        public CommentService(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
+        {
+        }
+
+        public async Task<CommentDTO> AddCommentAsync(CommentAddDTO commentAddDTO, int userId)
+        {
+            if (commentAddDTO == null)
+            {
+                throw null;
+            }
+
+            var photo = await _unit.Photos.GetByIdAsync(commentAddDTO.PhotoId);
+
+            if (photo == null)
+            {
+                throw new ValidationException("Photo was not found");
+            }
+
+            Comment comment = _mp.Map<Comment>(commentAddDTO);
+            
+            comment.UserId = userId;
+
+            await _unit.Comments.AddAsync(comment);
+            await _unit.SaveAsync();
+
+            var commentDTO = _mp.Map<CommentDTO>(comment);
+
+            var user = await _unit.UserManager.FindByIdAsync(comment.UserId.ToString());
+
+            commentDTO.UserName = user.UserName;
+
+            return commentDTO;
+        }
+
+        public async Task<CommentDTO> GetCommentAsync(int commentId)
+        {
+            var comment = await _unit.Comments.GetByIdAsync(commentId);
+
+            if (comment == null)
+            {
+                throw new ValidationException("Comment was not found");
+            }
+
+            var commentDTO = _mp.Map<CommentDTO>(comment);
+
+            commentDTO.UserName = comment.User.UserName;
+
+            return commentDTO;
+        }
+
+        public async Task<IEnumerable<CommentDTO>> GetCommentsAsync(int photoId)
+        {
+            var photo = await _unit.Photos.GetByIdAsync(photoId);
+
+            if (photo == null)
+            {
+                throw new ValidationException("Photo was not found");
+            }
+
+            var comments = photo.Comments;
+
+            var commentDTOs = new List<CommentDTO>();
+
+            foreach (var comment in comments)
+            {
+                var commentDTO = _mp.Map<CommentDTO>(comment);
+
+                commentDTO.UserName = comment.User.UserName;
+
+                commentDTOs.Add(commentDTO);
+            }
+
+            return commentDTOs;
+        }
+
+        public async Task RemoveCommentAsync(int commentId, int userId)
+        {
+            var comment = await _unit.Comments.GetByIdAsync(commentId);
+
+            if (comment == null)
+            {
+                throw new ValidationException("Comment was not found");
+            }
+
+            if (comment.UserId != userId)
+            {
+                throw new ValidationException("You don`t have permission to delete this comment");
+            }
+
+            _unit.Comments.Remove(comment);
+            await _unit.SaveAsync();
+        }
+    }
+}
---
+++ PhotoGallery.BLL/Services/UserService.cs
@@ -0,0 +1,168 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using PhotoGallery.BLL.Configs;
+using PhotoGallery.BLL.DTO;
+using PhotoGallery.BLL.Exceptions;
+using PhotoGallery.BLL.Interfaces;
+using PhotoGallery.DAL.Entities;
+using PhotoGallery.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.BLL.Services
+{
+    public class UserService : Service, IUserService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public UserService(IMapper mapper, IUnitOfWork unitOfWork, IOptions<JwtSettings> jwtSettings) : base(mapper, unitOfWork)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public async Task<IEnumerable<UserDTO>> GetUsersAsync()
+        {
+            var users = await _unit.Users.GetAll();
+
+            List<UserDTO> userDTOs = users.Select(user => MapUserToDTO(user)).ToList();
+
+            return userDTOs;
+        }
+
+        public async Task<UserDTO> GetUserAsync(int id)
+        {
+            User user = await _unit.Users.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                throw new ValidationException("User was not found");
+            }
+
+            UserDTO userDTO = MapUserToDTO(user);
+
+            return userDTO;
+        }
+
+        public async Task<UserDTO> GetUserByUserNameAsync(string userName)
+        {
+            if (userName == null)
+            {
+                throw null;
+            }
+
+            User user = await _unit.Users.GetByUserNameAsync(userName);
+
+            if (user == null)
+            {
+                throw new ValidationException("User was not found");
+            }
+
+            UserDTO userDTO = MapUserToDTO(user);
+
+            return userDTO;
+        }
+
+        public async Task CreateUserAsync(UserRegisterDTO data)
+        {
+            if (data == null)
+            {
+                throw null;
+            }
+
+            if (await _unit.UserManager.FindByEmailAsync(data.Email) != null)
+            {
+                throw new ValidationException("User with this email already exists");
+            }
+
+            if (await _unit.UserManager.FindByNameAsync(data.UserName) != null)
+            {
+                throw new ValidationException("User with this username already exists");
+            }
+
+            User user = new User
+            {
+                UserName = data.UserName,
+                Email = data.Email
+            };
+
+            await _unit.UserManager.CreateAsync(user, data.Password);
+            await _unit.UserManager.AddToRoleAsync(user, "User");
+        }
+
+        public async Task<(string, UserDTO)> LoginAsync(UserLoginDTO data)
+        {
+            User user = await _unit.UserManager.FindByNameAsync(data.UserName);
+
+            if (user == null || !await _unit.UserManager.CheckPasswordAsync(user, data.Password))
+            {
+                throw new ValidationException("Incorrect username or password");
+            }
+
+            UserDTO userDTO = MapUserToDTO(user);
+
+            return (GenerateJwtToken(userDTO), userDTO);
+        }       
+
+        public async Task DeleteUserAsync(int id)
+        {
+            User user = await _unit.UserManager.FindByIdAsync(id.ToString());
+
+            if (user == null)
+            {
+                throw new ValidationException("User was not found");
+            }
+
+            // Delete user`s likes before deleting user account
+            IEnumerable<Like> userLikes = await _unit.Likes.Find(like => like.UserId == id);
+            _unit.Likes.RemoveRange(userLikes);
+
+            // Delete user`s comments before deleting user account
+            IEnumerable<Comment> userComments = await _unit.Comments.Find(comment => comment.UserId == id);
+            _unit.Comments.RemoveRange(userComments);
+
+            await _unit.UserManager.DeleteAsync(user);
+        }
+
+        private string GenerateJwtToken(UserDTO userDTO)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userDTO.Id.ToString())
+            };
+
+            claims.AddRange(userDTO.Roles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            DateTime expires = DateTime.UtcNow.Add(_jwtSettings.LifeTime);
+
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.JwtKey);
+            var creds = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserDTO MapUserToDTO(User user)
+        {
+            var userDTO = _mp.Map<UserDTO>(user);
+
+            var roles = _unit.UserManager.GetRolesAsync(user).Result;
+
+            userDTO.Roles = roles.ToArray();
+
+            return userDTO;
+        }
+    }
+}
