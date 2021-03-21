44b1c54ad749754bbc2d1f570ab38ec518e92e5c
gtm417 <Tymofii_Hodik@Epam.com>
2021-03-21 11:47:39 +0200

    add service interfaces

---
+++ PhotoGallery.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PhotoGallery.BLL.DTO;
+
+namespace PhotoGallery.BLL.Interfaces
+{
+    public interface IUserService : IService
+    {
+        Task<IEnumerable<UserDTO>> GetUsersAsync();
+        Task CreateUserAsync(UserRegisterDTO data);
+        Task<(string, UserDTO)> LoginAsync(UserLoginDTO data);
+        Task<UserDTO> GetUserAsync(int id);
+        Task<UserDTO> GetUserByUserNameAsync(string userName);
+        Task DeleteUserAsync(int id);
+    }
+}
