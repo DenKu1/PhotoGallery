35b8cd69066372234f69875af83a20932ba0520c
gtm417 <Tymofii_Hodik@Epam.com>
2021-03-21 11:46:56 +0200

    add DTOs

---
+++ PhotoGallery.BLL/DTO/PhotoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PhotoGallery.BLL.DTO
+{
+    public class PhotoDTO
+    {
+        public int Id { get; set; }
+        //not shure if this prop can be useful
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public DateTime Created { get; set; }
+        public int Likes { get; set; }
+        public bool IsLiked { get; set; }
+    }
+}
